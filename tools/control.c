6e6a6251ce5ed76bdaf129be2afcf53a2ac9c1c6
Matt Schulte <schultetwin1@gmail.com>
2014-11-09 12:19:36 -0500

    [master] Added error checks and cleaned up
    The controller code was getting really messy with all the debugging. I cleaned it up. The i2c sending logic is in its
    own function now. I check that we actually get a ds4 object first. Also removed lots of the print outs and all
    unnecessary variables

--- tools/control.c
+++ tools/control.c
@@ -22,30 +22,40 @@ void intHandler(int dummy) {
   signal(SIGINT, SIG_DFL);
 }
 
-void print_usage() {
-  printf("ds4_connect\n");
+static int send_controls(mraa_i2c_context i2c, uint8_t* buf, int len) {
+  int total = len;
+  int to_send = 0;
+  while (len > 0) {
+    to_send = (len > 32) ? 32 : len;
+    result = mraa_i2c_write(i2c, buf, to_send);
+    buf += to_send;
+    len -= to_send;
+    if (result != MRAA_SUCCESS) {
+      mraa_result_print(result);
+      return -1;
+    }
+  }
+  return total;
 }
 
+// Returns time diff in terms of ms
 static long get_time_diff(const struct timeval start, const struct timeval end) {
   return ((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec)/1000.0) + 0.5;
 }
 
 int main(int argc, char** argv) {
   int bytes_read;
-  int num_found = 0;
-  int num_sent = 0;
   int ret;
   int i;
-  unsigned char data[11];
   const ds4_controls_t* controls;
   uint8_t buffer[2048];
-  uint8_t* d = buffer;
   mraa_result_t result;
   struct timeval start, end;
-  long seconds, useconds; 
 
-  ds4_t* ds4 = ds4_new();
 
+  /**
+   * Initialize I2C
+   */
   mraa_i2c_context i2c;
   i2c = mraa_i2c_init(6);
   if (!i2c) {
@@ -58,7 +68,15 @@ int main(int argc, char** argv) {
     mraa_result_print(result);
     return -1;
   }
-  
+
+  /**
+   * Initialize DS4
+   */
+  ds4_t* ds4 = ds4_new();
+  if (!ds4) {
+    printf("Can't initialize DS4 controller\n");
+    return -1;
+  }
 
   // scan for ds4
   ret = ds4_scan(ds4);
@@ -70,7 +88,7 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  // Connect to for one found
+  // Connect to the found DS4
   ret = ds4_connect(ds4);
   if (-1 == ret) {
     printf("Error Connecting to DS4 controller\n");
@@ -86,11 +104,10 @@ int main(int argc, char** argv) {
 
   gettimeofday(&start, NULL);
   
-  // read data
   signal(SIGINT, intHandler);
   while (keep_running) {
-    d = buffer;
     bytes_read = ds4_read(ds4);
+
     if (bytes_read == -1) {
       printf("Error Reading: %s\n", strerror(errno));
       break;
@@ -99,35 +116,31 @@ int main(int argc, char** argv) {
       printf("Controller disconnected\n");
       break;
     } else if (bytes_read != 79) {
+      // Bad data
       continue;
     }
+
     controls = ds4_controls(ds4);
+
+
     gettimeofday(&end, NULL);
-    if (get_time_diff(start, end) > 10) {
-      gettimeofday(&start, NULL);
-      ret = controller_data_to_control_command(controls, d);
-      printf("Raw: %d\n", controls->right_analog_y);
-      printf("Thrust: %f\n", (controls->right_analog_y > 127) ? (controls->right_analog_y - 127.0f) / 128.0f : 0.0f);
-      while (ret > 0) {
-        result = mraa_i2c_write(i2c, d, (ret > 32) ? 32 : ret);
-        d += (ret > 32) ? 32 : ret;
-        ret = ret - 32;
-        if (result != MRAA_SUCCESS) {
-          mraa_result_print(result);
-          mraa_i2c_stop(i2c);
-          i2c = mraa_i2c_init(6);
-          mraa_i2c_address(i2c, 4);
-          break;
-        }
-      }
-      for (i = 0; i < 55; i++) {
-        printf("%02x ", buffer[i]);
-      }
-      num_sent++;
-      printf("\n");
-      printf("Bytes: %d\n", 55);
-      printf("Num Sent: %d\n", num_sent);
+
+    if (get_time_diff(start, end) < 50) continue;
+    gettimeofday(&start, NULL);
+
+    ret = controller_data_to_control_command(controls, buffer);
+
+    ret = send_controls(i2c, buffer, ret);
+
+    // If error while sending, try reinitizalizing
+    if (ret == -1) {
+      mraa_i2c_stop(i2c);
+      i2c = mraa_i2c_init(6);
+      mraa_i2c_address(i2c, 4);
+      continue;
     }
+
+    printf("Bytes: %d\n", ret);
   }
 
   mraa_i2c_stop(i2c);
