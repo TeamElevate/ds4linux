8090d776d94b85b968b9260adbca70955344d9b7
Matt Schulte <schultetwin1@gmail.com>
2014-11-15 16:16:38 -0500

    Switched daemon to using shm code

--- tools/ds4_daemon.c
+++ tools/ds4_daemon.c
@@ -4,11 +4,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <shm.h>
+#include <ds4_data.h>
 #include <ds4_usb.h>
 #include <ds4_bt.h>
 #include <ds4.h>
 
-static int keep_running = 1;
+volatile sig_atomic_t keep_running;
 
 void intHandler(int sig) {
   keep_running = 0;
@@ -20,6 +22,21 @@ int main() {
   int ds4_conn_status;
   int bytes_read;
   const ds4_controls_t* controls;
+  key_t key;
+  ds4_shared_data_t* shared_data;
+
+  struct sigaction sa;
+  sa.sa_handler = intHandler;
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
+
+  shm_t* shm;
+
+
+  if (sigaction(SIGINT, &sa, NULL) == -1) {
+    printf("ERROR: Could not set signal handler\n");
+    return -1;
+  }
 
   ds4_t* ds4 = ds4_new();
   if (!ds4) {
@@ -27,8 +44,27 @@ int main() {
     return -1;
   }
 
+  // Setup shared memory
+  key = ftok("/opt/controller.ipc", 'R');
+  if (key == -1)  {
+    printf("ERROR: Failed to get key for shm\n");
+    return -1;
+  }
+
+  shm = shm_create(key);
+  if (!shm) {
+    printf("ERROR: Failed to create shm\n");
+    return -1;
+  }
+
+  shared_data = (ds4_shared_data_t*)shm_data(shm);
+  shm_lock(shm);
+  shared_data->controller_connected = 0;
+  shm_unlock(shm);
+
   // Daemon
   // Scans then connects to a DS4
+  keep_running = 1;
   while (keep_running) {
     // Scan until ds4 found
     do {
@@ -37,35 +73,51 @@ int main() {
 
     if (num_ds4_found < 0) {
       printf("ERROR: Error during DS4 Scanning: %s\n", strerror(errno));
-      return -1;
+      continue;
     }
 
     ds4_conn_status = ds4_connect(ds4);
     if (ds4_conn_status < 0) {
       printf("Error: Error during DS4 connection\n");
-      return -1;
+      continue;
     }
     if (ds4_conn_status == 0) {
-      // Lost connection
+      // Lost connection, try again
       ds4_disconnect(ds4);
       continue;
     }
 
     // Send and receive until disconnect 
     while (keep_running) {
-      if (!ds4_peek(ds4)) continue;
+      if (ds4_peek(ds4)) {
 
-      bytes_read = ds4_read(ds4);
-      if (bytes_read == -1) {
-        printf("Error Reading: %s\n", strerror(errno));
-        break;
-      } else if (bytes_read == 0) {
-        break;
-      } else if (bytes_read != 79) {
-        continue;
+        bytes_read = ds4_read(ds4);
+        if (bytes_read == 79) {
+          controls = ds4_controls(ds4);
+          shm_lock(shm);
+          shared_data->controller_connected = 1;
+          memcpy(&(shared_data->controls), controls, sizeof(ds4_controls_t));
+          shm_unlock(shm);
+        } else if (bytes_read == -1) {
+          shm_lock(shm);
+          shared_data->controller_connected = 0;
+          shm_unlock(shm);
+          printf("Error Reading: %s\n", strerror(errno));
+          break;
+        } else if (bytes_read == 0) {
+          shm_lock(shm);
+          shared_data->controller_connected = 0;
+          shm_unlock(shm);
+          // Disconnect
+          break;
+        }
       }
-      controls = ds4_controls(ds4);
     }
+    shm_lock(shm);
+    shared_data->controller_connected = 0;
+    shm_unlock(shm);
     ds4_disconnect(ds4);
   }
+  shm_detach(shm);
+  shm_destroy(&shm);
 }
