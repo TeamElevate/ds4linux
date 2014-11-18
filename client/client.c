5ad142cee412536c6337f0d45ab0d56c3ebf9b62
Matt Schulte <schultetwin1@gmail.com>
2014-11-18 11:12:05 -0500

    Sending from client side
    We can send data from the client now to set RGB and rumble. We can now also check for a connected DS4 controller

--- client/client.c
+++ client/client.c
@@ -61,22 +61,35 @@ void ds4_client_destroy(ds4_client_t** self_p) {
   }
 }
 
-ds4_controls_t* ds4_client_controls(ds4_client_t* self) {
+int ds4_client_connected(ds4_client_t* self) {
+  int rc;
+  assert(self);
+
+  rc = daemon_connect(self);
+  if (rc < 0) {
+    return 0;
+  }
+  close(self->sock);
+  return 1;
+}
+
+static int ds4_client_update(ds4_client_t* self) {
   int rc;
 
   assert(self);
   rc = daemon_connect(self);
   if (rc < 0) {
-    return NULL;
+    return -1;
   }
 
-  self->shared_data.send_data = 0;
   rc = send(self->sock, &self->shared_data, sizeof(ds4_shared_data_t), 0);
+  self->shared_data.send_data = 0;
+  self->shared_data.rumble = 0;
 
   if (rc != sizeof(ds4_shared_data_t)) {
     close(self->sock);
     self->sock = -1;
-    return NULL;
+    return -1;
   }
 
   rc = recv(self->sock, &self->shared_data, sizeof(ds4_shared_data_t), 0);
@@ -84,6 +97,20 @@ ds4_controls_t* ds4_client_controls(ds4_client_t* self) {
   if (rc != sizeof(ds4_shared_data_t)) {
     close(self->sock);
     self->sock = -1;
+    return -1;
+  }
+  close(self->sock);
+  self->sock = -1;
+  return 0;
+}
+
+const ds4_controls_t* ds4_client_controls(ds4_client_t* self) {
+  int rc;
+
+  assert(self);
+  self->shared_data.send_data = 0;
+  rc = ds4_client_update(self);
+  if (rc == -1) {
     return NULL;
   }
   return &self->shared_data.controls;
@@ -93,15 +120,22 @@ int ds4_client_rgb(ds4_client_t* self, uint8_t r, uint8_t g, uint8_t b) {
   int rc;
   assert(self);
 
-  // @TODO
+  self->shared_data.send_data = 1;
+  self->shared_data.r = r;
+  self->shared_data.g = g;
+  self->shared_data.b = b;
 
-  return 0;
+  rc = ds4_client_update(self);
+  return rc;
 }
 
 int ds4_client_rumble(ds4_client_t* self) {
+  int rc;
   assert(self);
 
-  // @TODO
+  self->shared_data.send_data = 1;
+  self->shared_data.rumble = 1;
 
-  return 0;
+  rc = ds4_client_update(self);
+  return rc;
 }
--- client/client.h
+++ client/client.h
@@ -5,11 +5,36 @@
 
 typedef struct _ds4_client_t ds4_client_t;
 
+/**
+ * Create a new ds4 client
+ *
+ * Check for a null ptr on return!
+ */
 ds4_client_t* ds4_client_new();
+
+/**
+ * Destructor for DS4 client
+ *
+ * pass in address of ds4_client_t*
+ */
 void ds4_client_destroy(ds4_client_t** self_p);
 
-ds4_controls_t* ds4_client_controls(ds4_client_t* self);
+/**
+ * Check is a DS4 is currently connected
+ * Returns 1 if connected, 0 if not
+ */
+int ds4_client_connected(ds4_client_t* self);
 
+/**
+ * Gets the latest ds4 controller data
+ */
+const ds4_controls_t* ds4_client_controls(ds4_client_t* self);
+
+/**
+ * Set the DS4 rgb and rumble
+ *
+ * Returns 0 on sucess, negative number on failure
+ */
 int ds4_client_rgb(ds4_client_t* self, uint8_t r, uint8_t g, uint8_t b);
 int ds4_client_rumble(ds4_client_t* self);
 #endif
