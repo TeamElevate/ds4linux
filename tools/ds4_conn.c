9b077b1b157aedb681046f35d538b4b398c892a4
Matt Schulte <schultetwin1@gmail.com>
2014-11-25 21:21:28 -0500

    Switched ds4_conn to use client

--- tools/ds4_conn.c
+++ tools/ds4_conn.c
@@ -5,20 +5,41 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#include <ds4_usb.h>
-#include <ds4_bt.h>
+#include <client.h>
 #include <ds4.h>
 
-static int keep_running = 1;
+volatile sig_atomic_t keep_running;
 
-void intHandler(int dummy) {
+void intHandler(int sig) {
+  struct sigaction sa;
+  sa.sa_handler = SIG_DFL;
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
   keep_running = 0;
-  signal(SIGINT, SIG_DFL);
+
+  sigaction(sig, &sa, NULL);
 }
 
-void print_usage() {
-  printf("ds4_connect\n");
+void set_handlers() {
+  struct sigaction sa;
+  sa.sa_handler = intHandler;
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
+
+  if (sigaction(SIGINT, &sa, NULL) == -1) {
+    printf("ERROR: Could not set signal handler\n");
+    exit(-1);
+  }
+  if (sigaction(SIGTERM, &sa, NULL) == -1) {
+    printf("ERROR: Could not set signal handler\n");
+    exit(-1);
+  }
+  if (sigaction(SIGHUP, &sa, NULL) == -1) {
+    printf("ERROR: Could not set signal handler\n");
+    exit(-1);
+  }
 }
 
 int main(int argc, char** argv) {
@@ -28,48 +49,22 @@ int main(int argc, char** argv) {
   unsigned char data[79];
   const ds4_controls_t* controls;
   int remote_disconnect = 0;
+  ds4_client_t* client = ds4_client_new();
 
-  ds4_t* ds4 = ds4_new();
-
-  // scan for ds4
-  ret = ds4_scan(ds4);
-  if (ret == 0) {
-    printf("No DS4 Controllers found\n");
-    return -1;
-  } else if (ret < 0) {
-    printf("Error while scanning\n");
+  if (!ds4_client_connected(client)) {
+    printf("ERROR: No client connected\n");
     return -1;
   }
 
-  // Connect to for one found
-  ret = ds4_connect(ds4);
-  if (-1 == ret) {
-    printf("Error Connecting to DS4 controller\n");
-    return -1;
-  } else if (ret == 0) {
-    printf("Error: Lost connection to DS4 controller\n");
-    ds4_disconnect(ds4);
-    return -1;
-  }
-
-  // read data
-  signal(SIGINT, intHandler);
   while (keep_running) {
-    bytes_read = ds4_read(ds4);
-    if (bytes_read == -1) {
-      printf("Error Reading: %s\n", strerror(errno));
-      break;
-    } else if (bytes_read == 0) {
-      // Connection closed
-      printf("Controller disconnected\n");
-      break;
-    } else if (bytes_read != 79) {
-      continue;
+    controls = ds4_client_controls(client);
+    if (!controls) {
+      printf("ERROR Reading\n");
+      return -1;
     }
-    controls = ds4_controls(ds4);
-    ds4_set_rgb(ds4, controls->left_analog_y, controls->left_analog_x, controls->right_analog_y);
+    ds4_client_rgb(client, controls->left_analog_y, controls->left_analog_x, controls->right_analog_y);
     if (controls->cross) {
-      ds4_rumble(ds4);
+      ds4_client_rumble(client);
     }
     printf("Left X: %6d Left Y: %6d Right X: %6d Right Y: %6d\n",
             controls->left_analog_x,
@@ -78,6 +73,6 @@ int main(int argc, char** argv) {
             controls->right_analog_y
           );
   }
-  ds4_disconnect(ds4);
+  ds4_client_destroy(&client);
   return 0;
 }
