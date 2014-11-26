e1c9c89ecd70248c2187529a9214d39a8deffe2f
Matt Schulte <schultetwin1@gmail.com>
2014-11-25 21:22:19 -0500

    Added a delete option

--- tools/ds4_link.c
+++ tools/ds4_link.c
@@ -1,15 +1,45 @@
+#include <getopt.h>
+
 #include <ds4_bt.h>
 
-int main(const int argc, const char* argv[]) {
+int main(int argc, char* argv[]) {
+  // getopt variables
+  int index;
+  int c;
+  int delete = 0;
+
   int rc;
-  if (argc != 3) {
+
+  opterr = 0;
+
+  while ((c = getopt(argc, argv, "d")) != -1) {
+    switch (c) {
+      case 'd':
+        delete = 1;
+        break;
+
+      default:
+        return -1;
+    }
+  }
+
+
+  if (argc != optind + 2 && !delete) {
     printf("./ds4_link <ds4 addr> <key>\n");
     return -1;
   }
+  if (argc != optind + 1 && delete) {
+    printf("./ds4_link <ds4 addr>\n");
+    return -1;
+  }
 
-  rc = set_bd_key(argv[1], argv[2]);
+  if (delete) {
+    rc = del_bd_key(argv[optind]);
+  } else {
+    rc = set_bd_key(argv[optind], argv[optind + 1]);
+  }
   if (rc != 0) {
-    printf("ERROR: Not able to set set bluetooth key\n");
+    printf("ERROR: Not able to maniuplate bluetooth key\n");
     return -1;
   }
 
