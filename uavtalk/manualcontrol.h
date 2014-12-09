7b8fae82e295c94019c4c83594e6eaaf8bc77e23
Matt Schulte <schultetwin1@gmail.com>
2014-12-08 20:03:36 -0500

    Added comments to manualcontrol.h

--- uavtalk/manualcontrol.h
+++ uavtalk/manualcontrol.h
@@ -4,6 +4,9 @@
 #include <ds4.h>                 /* ds4_controls_t */
 #include <inttypes.h>            /* uint8_t  */
 
+/**
+ * Take DS4 controller data and turn it into uavtalk manual control commands
+ */
 uint16_t controller_data_to_control_command(const ds4_controls_t*, uint8_t*);
 
 #endif
